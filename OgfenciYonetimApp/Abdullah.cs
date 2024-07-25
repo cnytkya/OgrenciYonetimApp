ee131529e55d38f98155b7d483408b4621316e03
cnytkya <kayacuneyt2542@gmail.com>
2024-07-25 22:29:27 +0300

    Added new classes: Gizem and Abdullah

---
+++ OgfenciYonetimApp/Abdullah.cs
@@ -0,0 +1,9 @@
+﻿namespace OgfenciYonetimApp
+{
+    public class Abdullah
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ OgfenciYonetimApp/Gizem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgfenciYonetimApp
+{
+    public class Gizem
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+}
