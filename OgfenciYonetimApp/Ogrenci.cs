f41214d725d0f4574d95c6c668b8f3a4509e5a48
cnytkya <kayacuneyt2542@gmail.com>
2024-07-25 22:24:53 +0300

    Add project files.

---
+++ OgfenciYonetimApp/Ogrenci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgfenciYonetimApp
+{
+    public class Ogrenci //internal erişim belirteci, Ogrenci sınıfının yalnızca aynı assembly (derleme) içinde erişilebilir olduğunu belirtir.
+    {//Ogrenci Sınıfın adıdır. Bu sınıf öğrenci ile ilgili bilgileri tutar.
+     //property
+        public string Ad { get; set; } //public erişim belirteci, Ad özelliğinin diğer sınıflar ve kod bölümleri tarafından erişilebilir olduğunu belirtir.
+
+        public string Soyad { get; set; }
+        public int Yas { get; set; }
+        public int Numara { get; set; }
+
+        //Bu kod parçası, Ogrenci adında bir sınıf tanımlar ve bu sınıf içinde dört özellik (property) bulunur: Ad, Soyad, Yas, ve Numara. Her bir özellik public erişim belirtecine sahiptir, bu da bu özelliklerin sınıf dışından erişilebilir ve değiştirilebilir olduğu anlamına gelir. Ad ve Soyad özellikleri string veri tipindedir, yani metin değerlerini tutar. Yas ve Numara özellikleri ise int veri tipindedir, yani tamsayı değerlerini tutar.
+    }
+}
---
+++ OgfenciYonetimApp/Program.cs
@@ -0,0 +1,102 @@
+﻿using OgfenciYonetimApp;
+using System.Threading.Channels;
+
+List<Ogrenci> ogrenciler = new List<Ogrenci>();
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Öğrenci Tönetim Uygulaması");
+    Console.WriteLine("1-Öğrenci Ekle");
+    Console.WriteLine("2-Öğrenci Güncell");
+    Console.WriteLine("3-Öğrenci Sil");
+    Console.WriteLine("4-Öğrencileri Listele");
+    Console.WriteLine("5-Çıkış Yap");
+    Console.Write("Seçimini Yap: ");
+    int secim = int.Parse(Console.ReadLine());
+
+    switch (secim)
+    {
+        case 1:
+            OgrenciEkle(ogrenciler);
+            break;
+
+        case 2:
+            OgrenciGuncelle(ogrenciler);
+            break;
+
+        case 3:
+            OgrenciSil(ogrenciler);
+            break;
+
+        case 4:
+            OgrencileriListele(ogrenciler);
+            break;
+
+        case 5:
+            Console.WriteLine("Çıkış Yapılıyor...");
+            return;
+
+        default:
+            Console.WriteLine("Geçersiz Seçim!");
+            break;
+    }
+    Console.WriteLine("Devam etmek için bir tuşa basın.");
+    Console.ReadKey();
+}
+
+static void OgrenciEkle(List<Ogrenci> ogrenciler)
+{
+    Console.Write("Öğrenci Numarası: ");
+    int numara = int.Parse(Console.ReadLine());
+    Console.Write("Öğrenci Adı: ");
+    string ad = Console.ReadLine();
+    Console.Write("Öğrenci Soyadı: ");
+    string soyad = Console.ReadLine();
+    Console.Write("Öğrenci Yaşı: ");
+    int yas = int.Parse(Console.ReadLine());
+
+    ogrenciler.Add(new Ogrenci { Numara = numara, Ad = ad, Soyad = soyad, Yas = yas });
+    Console.WriteLine("Öğrenci başarıyla eklendi.");
+}
+static void OgrenciGuncelle(List<Ogrenci> ogrenciler)
+{
+    Console.WriteLine("Güncellemek istediğiniz öğrencinin numarasını giriniz: ");
+    int numara = int.Parse(Console.ReadLine());
+
+    Ogrenci ogrenci = ogrenciler.Find(o => o.Numara == numara);
+    if (ogrenci != null)
+    {
+        Console.WriteLine("Yeni Adı: ");
+        ogrenci.Ad = Console.ReadLine();
+        Console.WriteLine("Yeni Soyadı: ");
+        ogrenci.Soyad = Console.ReadLine();
+        Console.WriteLine("Yeni Yaşı: ");
+        ogrenci.Yas = int.Parse(Console.ReadLine());
+        if (true)
+        {
+            string deger = "Öğrenci Bilgileri Başarıyla Güncellenmiştir.";
+            Console.WriteLine(deger);
+        }
+    }
+    else
+        Console.WriteLine("Öğrenci bulunamadı.");
+}
+static void OgrenciSil(List<Ogrenci> ogrenciler)
+{
+    Console.WriteLine("Silmek istediğiniz öğrencinin numarasını giriniz: ");
+    int numara = int.Parse(Console.ReadLine());
+
+    Ogrenci ogrenci = ogrenciler.Find(o => o.Numara == numara);
+    if (numara != null)
+    {
+        ogrenciler.Remove(ogrenci);
+        Console.WriteLine("Öğrenci başarıyla silinmiştir.");
+    }
+    else Console.WriteLine("Öğrenci bulunamadı");
+}
+static void OgrencileriListele(List<Ogrenci> ogrenciler)
+{
+    Console.WriteLine("Öğrenci Listesi:");
+    foreach (Ogrenci o in ogrenciler)
+        Console.WriteLine($"Ad: {o.Ad} \nSoyad: {o.Soyad} \nYaş {o.Yas} \nNumara: {o.Numara}");
+}
